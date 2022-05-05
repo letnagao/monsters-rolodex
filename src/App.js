f3e00c27d4945f94bd4457cb8f8cd4773d20b3c9
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-05 10:36:21 -0300

    Changing search field from the full list
    Storing Original Data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '' 
     };
     console.log('constructor');
   }
@@ -28,6 +29,12 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input 
@@ -35,20 +42,13 @@ class App extends Component {
           type='search' 
           placeholder='search monsters'  
           onChange={(event) => {
-            console.log({ startingArray: this.state.monsters });
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-            
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters}
-            }, () => {
-              console.log({ endingArray: this.state.monsters });
+              return { searchField };
             });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
