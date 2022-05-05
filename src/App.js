1eac95dd2dc1d3773c3010788787a85362f4b6d7
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-05 11:05:37 -0300

    Add CardList Component
    Improving render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -28,11 +29,21 @@ class App extends Component {
     );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -41,20 +52,16 @@ class App extends Component {
           className = 'search-box' 
           type='search' 
           placeholder='search monsters'  
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
               </div>
             );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
